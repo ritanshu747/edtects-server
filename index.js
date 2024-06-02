7c8b6a08a7a60d480458a902d17a2adcb980bd2a
ritanshu747 <73697511+ritanshu747@users.noreply.github.com>
2024-06-02 20:44:59 +0530

    testing commit

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
     cors({
-        origin: ['https://studynotion-dev-abhi.vercel.app', 'http://localhost:3000'],
+        origin: ['https://edtech-client-1.onrender.com/', 'http://localhost:3000'],
         credentials: true,
     })
 )
